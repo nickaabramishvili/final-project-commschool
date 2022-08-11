2cf4714744323ab5c76a0e1d895febe1c525a7d4
nikabramishvili <n.abramishvili@area.ge>
2022-08-11 23:37:02 +0400

    prog auth done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
-import { SignUpComponent } from './auth/sign-up/sign-up.component';
-import { VerifyEmailComponent } from './auth/verify-email/verify-email.component';
+
 import { DashboardComponent } from './feature/dashboard/dashboard.component';
 import { AuthGuard } from './shared/guards/auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
-  { path: 'sign-in', component: SignInComponent },
-  { path: 'sign-up', component: SignUpComponent },
+  { path: '', redirectTo: 'auth/sign-in', pathMatch: 'full' },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+
   {
     path: 'dashboard',
     component: DashboardComponent,
     canActivate: [AuthGuard],
   },
-  { path: 'verify-email', component: VerifyEmailComponent },
+  { path: '**', component: SignInComponent },
 ];
 
 @NgModule({
